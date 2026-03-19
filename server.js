02175c86bfd4d9abffe0a5f53005808c877f9522
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-19 15:35:02 -0400

    created Schemas

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ import express  from  'express'
 
 import './config/connection.js'
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json())
+
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
