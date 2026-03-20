566e78d7bdec3d330d72a0d95caa2d674d082501
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-20 09:16:26 -0400

    working on authorization

--- models/user.js
+++ models/user.js
@@ -1,3 +1,26 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from "bcrypt"
 
+const userSchema = new Schema({
+    username:{
+         type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    },
+
+    email:{
+        type: String,
+    required: true,
+    unique: true,
+    match: [/.+@.+\..+/, "Must use a valid email address"],
+    },
+    password:{
+        type: String,
+    minlength: 5,
+    required: true
+    }
+});
+
+const User = mongoose.model("User",userSchema);
+export default User;
